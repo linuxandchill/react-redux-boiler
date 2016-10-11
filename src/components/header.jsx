374c9829710f0ede538bdac80ec608858cb8dc25
ck <thecurrencykid@gmail.com>
2016-10-11 15:14:25 -0400

    bs jquery

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,8 +19,6 @@ class Header extends Component{
             <div id="navbar" className="navbar-collapse collapse">
               <ul className="nav navbar-nav">
                 <li className="active"><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
                 <li className="dropdown">
                   <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
                   <ul className="dropdown-menu">
@@ -35,8 +33,6 @@ class Header extends Component{
                 </li>
               </ul>
               <ul className="nav navbar-nav navbar-right">
-                <li className="active"><a href="./">Default <span className="sr-only">(current)</span></a></li>
-                <li><a href="../navbar-static-top/">Static top</a></li>
                 <li><a href="../navbar-fixed-top/">Sign in</a></li>
               </ul>
             </div>
