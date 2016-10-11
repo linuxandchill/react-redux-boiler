14b956410c71542ba257d150862d9a33ccf0b766
ck <thecurrencykid@gmail.com>
2016-10-11 14:57:01 -0400

    router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {Router, Route, IndexRoute, browserHistory } from 'react-router'; 
 import './index.css';
 
 import App from './App';
@@ -12,6 +13,9 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} > 
+      <Route path="/" component={App} >
+      </Route> 
+    </Router> 
   </Provider>
   , document.getElementById('root'));
